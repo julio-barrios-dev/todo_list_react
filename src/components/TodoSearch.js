ca29d709a85f39e4f5a0451b5a27c643b04d1370
juliobarrios96 <julio.barrios.9618@gmail.com>
2022-03-27 01:16:43 -0400

    Manejo del estado

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,18 +1,24 @@
-import react from "react";
+import React from "react";
 import './styles/TodoSearch.css'
 
 function TodoSearch(){
+    const [searchValue, setSearhValue ] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearhValue(event.target.value);
     };
 
-    return(
+    return [
         <input 
             className="TodoSearch" 
-            placeholder="Cebolla" 
+            placeholder="Cebolla"
+            value={searchValue}
             onChange={onSearchValueChange}    
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+        
+    ];
 }
 
 export {TodoSearch};
