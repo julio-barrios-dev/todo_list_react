fffa26c6d881f4fc9bcda46081dfbe0fdadef146
juliobarrios96 <julio.barrios.9618@gmail.com>
2022-04-18 12:58:41 -0300

    Ejemplo de composición

--- src/App/index.js
+++ src/App/index.js
@@ -11,17 +11,61 @@ import { AppUI } from "./AppUI.js";
   completed: true}
 ]; */
 
-//Custon hooks para Local Storage
-
-
-
 function App() {
+  const [state, setState] = React.useState('Estado compartido');
+
+  return (
+      <React.Fragment>
+        <TodoHeader>
+          <TodoCounter/>
+          <TodoSearch/>
+        </TodoHeader>
+
+        <TodoList>
+          <TodoItem state={state} />
+        </TodoList>
+      </React.Fragment>
+  )  
+}
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  )
+}
+function TodoList({ children }) {
+  return (
+    <section className="TodoList-container">
+      {children}
+    </section>
+  )
+}
+function TodoCounter() {
+
+  return (
+    <p>TodoCounter</p>
+  )
+}
+function TodoSearch() {
+  return (
+    <p>TodoSearch</p>
+  )
+}
+function TodoItem({ state }) {
+  return (
+    <p>TodoItem {state}</p>
+  )
+}
+
+
+/* function App() {
   
   return (
     <TodoProvider>
       <AppUI/>
     </TodoProvider>
   );
-};
+}; */
 
 export default App;
