c6f2e8481244f6368683b2fd828e1856b6386385
juliobarrios96 <julio.barrios.9618@gmail.com>
2022-04-20 14:15:25 -0300

    Creando primer HOF

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/';
+// import App from './App';
 
+function App(props) {
+  return (
+    <h1>{props.saludo}, {props.nombre}!</h1>
+  );
+}
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      )
+    }
+  }
+}
+
+
+const AppWithSaludo = withSaludo(App)("Holaa");
 
 ReactDOM.render(
-  <App />,
+  <AppWithSaludo nombre="Mau"  />,
+  // <App saludo="Hola" nombre="Mau" />,
   document.getElementById('root')
 );
 
